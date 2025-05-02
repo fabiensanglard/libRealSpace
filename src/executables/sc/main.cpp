5a405a31d38666c16680344122dc24350578b83f
Rémi Léonard <rpy.leonard@gmail.com>
2025-05-02 09:56:46 +0200

    Enhance: Update FLOPPY definition to 0 and reorganize asset path initialization for improved clarity and maintainability

--- src/executables/sc/main.cpp
+++ src/executables/sc/main.cpp
@@ -7,7 +7,7 @@
 //
 
 #include "../../strike_commander/precomp.h"
-#define FLOPPY 1
+#define FLOPPY 0
 //Here are all the mean subsystems interacting together
 SCState         GameState;
 GameEngine      *Game;
@@ -45,7 +45,25 @@ int main(int argc, char* argv[]) {
         Assets.ReadISOImage("./SC.DAT");
         Assets.init(cdTreFiles);
     }
-    
+    Assets.intel_root_path = "..\\..\\DATA\\INTEL\\";
+    Assets.mission_root_path = "..\\..\\DATA\\MISSIONS\\";
+    Assets.object_root_path = "..\\..\\DATA\\OBJECTS\\";
+    Assets.sound_root_path = "..\\..\\DATA\\SOUNDS\\";
+    Assets.texture_root_path = "..\\..\\DATA\\TXM\\";
+    Assets.gameflow_root_path = "..\\..\\DATA\\GAMEFLOW\\";
+
+    Assets.gameflow_filename = Assets.gameflow_root_path+"GAMEFLOW.IFF";
+    Assets.optshps_filename = Assets.gameflow_root_path+"OPTSHPS.PAK";
+    Assets.optpals_filename = Assets.gameflow_root_path+"OPTPALS.PAK";
+    Assets.optfont_filename = Assets.gameflow_root_path+"OPTFONT.IFF";
+    Assets.navmap_filename = "..\\..\\DATA\\COCKPITS\\NAVMAP.IFF";
+    Assets.conv_pak_filename = Assets.gameflow_root_path+"CONVSHPS.PAK";
+    Assets.option_filename = Assets.gameflow_root_path+"OPTIONS.IFF";
+    Assets.conv_data_filename = Assets.gameflow_root_path+"CONVDATA.IFF";
+    Assets.conv_pal_filename = Assets.gameflow_root_path+"CONVPALS.PAK";
+    Assets.txm_filename = Assets.texture_root_path+"TXMPACK.PAK";
+    Assets.acc_filename = Assets.texture_root_path+"ACCPACK.PAK";
+    Assets.convpak_filename = Assets.gameflow_root_path+"CONV.PAK";
     Mixer.init(&Assets);
     FontManager.init(&Assets);
 
